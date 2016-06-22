ccf91ccde23a1ea21e455a08c7d8edf6151a8a07
Michał Marcinkowski <189726@edu.p.lodz.pl>
2016-06-23 01:56:30 +0200

    Add controllers

---
+++ src/main/java/com/zzpj/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.zzpj.controller;
+
+import com.zzpj.domain.UserCreateForm;
+import com.zzpj.domain.validator.UserCreateFormValidator;
+import com.zzpj.service.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.NoSuchElementException;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+    private final UserCreateFormValidator userCreateFormValidator;
+
+    @Autowired
+    public UserController(UserService userService, UserCreateFormValidator userCreateFormValidator) {
+        this.userService = userService;
+        this.userCreateFormValidator = userCreateFormValidator;
+    }
+
+    @InitBinder("form")
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(userCreateFormValidator);
+    }
+
+    @RequestMapping("/user/{id}")
+    public ModelAndView getUserPage(@PathVariable Long id) {
+        return new ModelAndView("user", "user", userService.getUserById(id)
+                .orElseThrow(() -> new NoSuchElementException(String.format("User=%s not found", id))));
+    }
+
+    @RequestMapping(value = "/user/create", method = RequestMethod.GET)
+    public ModelAndView getUserCreatePage() {
+        return new ModelAndView("user_create", "form", new UserCreateForm());
+    }
+
+    @RequestMapping(value = "/user/create", method = RequestMethod.POST)
+    public String handleUserCreateForm(@Valid @ModelAttribute("form") UserCreateForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "user_create";
+        }
+        try {
+            userService.create(form);
+        } catch (DataIntegrityViolationException e) {
+            bindingResult.reject("email.exists", "Email already exists");
+            return "user_create";
+        }
+        return "redirect:/";
+    }
+
+}
