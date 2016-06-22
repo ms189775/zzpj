2bb2662057d69ed987248aa4a74ab757ce03c55e
Marcin Socha <189775@edu.p.lodz.pl>
2016-06-22 16:41:53 +0200

    clean up

--- src/main/java/com/zzpj/controller/UrlShortenerRequest.java
+++ src/main/java/com/zzpj/controller/UrlShortenerRequest.java
@@ -4,9 +4,9 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class UrlShortenerRequest {
-    @Size(min = 5, max = 1024, message="URL size must be between {min} and {max} characters long.")
+    @Size(min = 5, max = 1024, message = "URL size must be between {min} and {max} characters long.")
     private String url;
-    @Pattern(regexp = "^[a-zA-Z0-9_-]{0,64}$", message="URL name size must be up to {max} characters long and has only A-Z, a-z, 0-9, - and _ characters.")
+    @Pattern(regexp = "^[a-zA-Z0-9_-]{0,64}$", message = "URL name size must be up to {max} characters long and has only A-Z, a-z, 0-9, - and _ characters.")
     private String urlName;
 
     public String getUrl() {
