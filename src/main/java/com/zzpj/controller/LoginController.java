39ab777c6f96d1cdd8675f0abcc2c8aa078a4434
Marcin Socha <189775@edu.p.lodz.pl>
2016-06-23 20:16:52 +0200

    Fix minor issues

--- src/main/java/com/zzpj/controller/LoginController.java
+++ src/main/java/com/zzpj/controller/LoginController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @Controller
 public class LoginController {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
+    @PreAuthorize("isAnonymous()")
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView getLoginPage(@RequestParam Optional<String> error) {
         LOGGER.debug("Getting login page, error={}", error);
